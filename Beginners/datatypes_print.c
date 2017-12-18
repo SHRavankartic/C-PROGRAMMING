d27d2a025326b04698e786caad3c0e6fe1105c0d
SHRavankartic <7karthics@gmail.com>
2017-12-18 12:42:23 +0530

    Create datatypes_print.c

---
+++ Beginners/datatypes_print.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void)
+{
+	char A[]="Sachin Ramesh Tendulkar";
+	char B[]="Little Master";
+	int rodi=18426;
+	float ravg=44.83;
+	int m=463;
+	printf("%s\n%s\n%d\n%f\n%d",A,B,rodi,ravg,m);
+}
