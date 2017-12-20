1c02cd3906a3ba5ff324e0222d1fbb6550308680
Shravan Karthic A <7karthics@gmail.com>
2017-12-20 15:57:05 +0530

    Update RotatingArray.c

--- ClassAssignment/RotatingArray.c
+++ ClassAssignment/RotatingArray.c
@@ -13,19 +13,19 @@ void l2(int a[], int d, int n)
 {
 	int i;
 	for (i = 0; i < d; i++)
-	l1(a, n);
+		l1(a, n);
 }
 void l1(int a[], int n)
 {
 	int i, temp;
 	temp = a[0];
 	for (i = 0; i < n-1; i++)
-	a[i] = a[i+1];
+		a[i] = a[i+1];
 	a[i] = temp;
 }
 void display(int a[], int size)
 {
 	int i;
 	for(i = 0; i < size; i++)
-	printf("%d ", a[i]);
+		printf("%d ", a[i]);
 }
